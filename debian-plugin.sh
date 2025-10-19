e35b924cc9dfbaf7961913cfe18e02a95f8ca2a0
rmz3t-dev <rmz3t.developers@gmail.com>
2025-10-19 20:28:54 +0700

    Update debian-plugin.sh

--- debian-plugin.sh
+++ debian-plugin.sh
@@ -4,9 +4,9 @@
 DISTRO_NAME="Debian (bookworm)"
 DISTRO_COMMENT="Previous version (aarch64 & arm only)"
 
-TARBALL_URL['aarch64']="https://github.com/termux/proot-distro/releases/download/v4.17.3/debian-trixie-aarch64-pd-v4.17.3.tar.xz"
+TARBALL_URL['aarch64']="https://github.com/termux/proot-distro/releases/download/v4.17.3/debian-bookworm-aarch64-pd-v4.17.3.tar.xz"
 TARBALL_SHA256['aarch64']="3834a11cbc6496935760bdc20cca7e2c25724d0cd8f5e4926da8fd5ca1857918"
-TARBALL_URL['arm']="https://github.com/termux/proot-distro/releases/download/v4.17.3/debian-trixie-arm-pd-v4.17.3.tar.xz"
+TARBALL_URL['arm']="https://github.com/termux/proot-distro/releases/download/v4.17.3/debian-bookworm-arm-pd-v4.17.3.tar.xz"
 TARBALL_SHA256['arm']="99bcba87d8d1c66c0de06259ac0a270eb0a20f8b4af39beb0705d28846d78b90"
 
 distro_setup() {
